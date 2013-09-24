7fd2375deb43d751375453de461ba69f34609ce0
howarto <howarto@ymail.com>
2013-09-24 10:28:38 +0200

    Arreglado problema con los templates. Era error con los staticfiles

---
+++ proyecto/app/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.forms import ModelForm
+from models import *
+
+class ProyectoForm(ModelForm):
+    
+    class Meta:
+        model = Proyecto
+        exclude = ("participantes",)
+        
